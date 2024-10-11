20f0ef76b594e6e518cad4257805cb7ed0189783
Max Lindström <max941@live.se>
2024-10-11 13:31:32 +0200

    Add REST API endpoint for getting all products from a category

--- src/main/java/org/example/rest/WarehouseResource.java
+++ src/main/java/org/example/rest/WarehouseResource.java
@@ -5,6 +5,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.example.entities.Category;
 import org.example.entities.Product;
 import org.example.service.Warehouse;
 
@@ -33,5 +34,18 @@ public class WarehouseResource {
             return Response.ok(product.get()).build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @GET
+    @Path("/products/category/{category}")
+    public Response getProductsByCategory(@PathParam("category") String category) {
+        Category cat;
+        try  {
+            cat = Category.valueOf(category.toUpperCase());
+            return Response.ok(warehouse.getProductsByCategory(cat)).build();
+        }
+        catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
     }
 }
